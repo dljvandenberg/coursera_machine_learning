ab23a579afbf7b9607076371ad2172597fecc119
dljvandenberg <dljvandenberg@gmail.com>
2015-10-25 20:10:23 +0100

    modified R code + finished version of report

--- prediction_assignment/prediction.R
+++ prediction_assignment/prediction.R
@@ -7,6 +7,7 @@ set.seed(1234)
 dir.work <- "~/git/practical_machine_learning/prediction_assignment/data"
 file.train <- "pml-training.csv"
 file.predict <- "pml-testing.csv"
+# Set p small to speed up training process (with risk of lower accuracy)
 p.training <- 0.1
 
 # Libraries
@@ -24,33 +25,20 @@ df.training <- df.train.file[m.train,]
 df.testing <- df.train.file[-m.train,]
 df.predicting <- df.predict.file
 
-# Clear obsolete objects from memory
-rm(df.predict.file, df.train.file)
-
 
 ## Exploring raw data sets
 
-# dim(df.training)
-# # Training set consists of .. measurements with 160 variables
-# dim(df.testing)
-# # Validating set consists of .. measurements with 160 variables
-# names(df.training)
-# 
-# histogram(df.training$user_name)
-# histogram(df.testing$user_name)
-# # 6 persons have participated in experiments (variable: user_name), all 6 are present in training and test sets
-# 
-# histogram(df.training$classe)
-# # Outcome variable that we want to predict is 'classe', factor variable with 5 different levels A, B, C, D, E
-# 
-# head(df.training)
-# # Training set contains time series measurements with time variables raw_timestamp_part_1, raw_timestamp_part_2, cvtd_timestamp
-# # and measured in time windows denoted by num_window. At the end of each window aggregate data is calculated
-# # (denoted by new_window=yes) and this is incorporated into the same dataset.
-# 
-# # Some plots based on training
-# qplot(num_window, pitch_belt, data=df.training, colour=classe, pch=user_name)
-# qplot(accel_forearm_x, accel_forearm_z, data=df.training, colour=classe, pch=user_name)
+# Check data sets
+dim(df.training)
+dim(df.testing)
+names(df.training)
+head(df.training)
+lapply(df.training, class)
+lapply(df.testing, class)
+
+# Some plots based on training
+qplot(num_window, pitch_belt, data=df.training, colour=classe, pch=user_name)
+qplot(accel_forearm_x, accel_forearm_z, data=df.training, colour=classe, pch=user_name)
 
 
 ## Data Cleaning
@@ -64,59 +52,17 @@ df.testing <- subset(df.testing, new_window=="no", select=-c(X, user_name, raw_t
 df.training <- df.training[,colSums(is.na(df.training))<nrow(df.training)]
 df.testing <- df.testing[,colSums(is.na(df.testing))<nrow(df.testing)]
 
-# # Verify class of variables in dataframe
-# lapply(df.training, class)
-# lapply(df.testing, class)
-
 
 ## Model 1: rpart
 
 # rpart model gave poor results (accuracy=0.5), not used anymore
-
-# # Train model
-# model1 <- train(factor(classe) ~ ., method="rpart", data=df.training)
-# 
-# # Confusion table and accuracy for training set
-# list.training.results1 <- predict(model1, newdata=df.training)
-# table(df.training$classe, list.training.results1)
-# sum(df.training$classe == list.training.results1) / length(df.training$classe)
-# 
-# # Confusion table and accuracy for testing set
-# list.testing.results1 <- predict(model1, newdata=df.testing)
-# table(df.testing$classe, list.testing.results1)
-# sum(df.testing$classe == list.testing.results1) / length(df.testing$classe)
-# 
-# # Most important variables
-# varImp(model1)
-# 
-# # Save/load model to/from file
-# saveRDS(model1, "model_rpart.rds")
-# # model1 <- readRDS("model_rpart.rds")
+#model.rpart <- train(factor(classe) ~ ., method="rpart", data=df.training)
 
 
 ## Model 2: gbm
 
 # gbm model gave memory allocation error, not used for now
-
-# # Train model
-# model2 <- train(factor(classe) ~ ., method="gbm", data=df.training)
-# 
-# # Confusion table and accuracy for training set
-# list.training.results2 <- predict(model2, newdata=df.training)
-# table(df.training$classe, list.training.results2)
-# sum(df.training$classe == list.training.results2) / length(df.training$classe)
-# 
-# # Confusion table and accuracy for testing set
-# list.testing.results2 <- predict(model2, newdata=df.testing)
-# table(df.testing$classe, list.testing.results2)
-# sum(df.testing$classe == list.testing.results2) / length(df.testing$classe)
-# 
-# # Most important variables
-# varImp(model2)
-# 
-# # Save/load model to/from file
-# saveRDS(model2, "model_gbm.rds")
-# # model2 <- readRDS("model_gbm.rds")
+#model.gbm <- train(factor(classe) ~ ., method="gbm", data=df.training)
 
 
 ## Model 3: rf
